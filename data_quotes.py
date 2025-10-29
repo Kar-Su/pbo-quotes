1a4bc485b5ab6c078811bb029488e9b534fcd045
Helmi <rumahhelmi@gmail.com>
2025-10-30 00:34:20 +0800

    chore: Membuat struktur projek
    Signed-off-by: Helmi <rumahhelmi@gmail.com>

---
+++ data_quotes.py
@@ -0,0 +1,34 @@
+"""PANDUAN
+data_quotes.py
+
+Tempat penyimpanan data quotes
+dimana setiap jenis quote ada 10 quote (Jangan lebih dan kurang).
+
+
+Variable:
+    agama (list[str...])        : Tempat penyimpanan quote agama
+    percintaan (list[str...])   : Tempat penyimpanan quote percintaan
+    politik (list[str...])      : Tempat penyimpanan quote politik
+"""
+
+
+"""@@@
+Task 1
+
+Mencari 10 quote agama dan memasukkannya kedalam list
+"""
+agama = ["agama1", "agama2"]
+
+"""@@@
+Task 2
+
+Mencari 10 quote percintaan dan memasukkannya kedalam list
+"""
+percintaan = ["cinta1", "cinta2"]
+
+"""@@@
+Task 3
+
+Mencari 10 quote politik dan memasukkannya kedalam list
+"""
+politik = ["politik1", "politik2"]
---
+++ quotes.py
@@ -0,0 +1,86 @@
+"""PANDUAN
+quotes.py
+
+Sebuah file utama yang menjalankan quotes generator
+user hanya perlu menjalankan main.py
+"""
+
+from random import randint
+"""@@@
+Task Tambahan (1, 2, 3)
+
+Untuk rekan-rekan yang mengerjakan dibagian data_quotes.py
+tolong import hasil kerjaan kalian kedalam main.py
+"""
+from data_quotes import (...)
+
+def main():
+    """PANDUAN
+    Fungsi main
+    
+    Sebuah fungsi utama dari quotes generator
+    
+    variable:
+        jenis_quotes(str.lower())   : Sebuah inputan user untuk menentukan jenis quote
+        data(list[str...])          : data quote dari data_quotes.py yang disesuaikan dengan jenis quote
+        iterasi(int)                : Sebuah inputan user untuk menentukan berapa banyak quote yang akan ditampilkan
+        random_generator(int)       : Sebuah angka integer yang ditentukan dari random generator(randint)
+    """
+    
+    
+    """@@@
+    Task 4
+    
+    Membuat sebuah kode untuk menentukan jenis quote menggunakan switch case,
+    dan menentukan data mana yang akan dipakai,
+    
+    Fitur:
+        Huruf besar dan kecil tidak mempengaruhi hasil inputannya.
+        Contoh Agama,AGama,PERCIntaan,politik,POLITIK.
+        
+    Note:
+        Metode boleh diubah, akan tetapi output kerjaannya harus sama.
+    """
+    jenis_quotes = ...
+    match ...:
+        case "...":
+            data = ...
+        case "...":
+            data = ...
+        case "...":
+            data = ...
+        case _:
+            data = [None]
+            print("Jenis yang anda ketikkan tidak ada [agama, percintaan, politik]")
+    
+    
+    """@@@
+    Task 5
+    
+    Membuat sebuah kode untuk menentukan seberapa banyak quote yang muncul.
+    
+    Fitur:
+        Setiap iterasi berjalan, maka quotenya berbeda - beda
+        menggunakan random generator(randint)
+    """
+    iterasi = ...
+    for i in ...:
+        print(f"Quotes ke ...")
+        random_generator = ...
+        print(...)
+    
+if __name__ == "__main__":
+    """PANDUAN
+    if __name__ == "__main__":
+    
+    Melakukan cek apakah nama file ini di sistem python itu "__main__".
+    ketika namanya itu "__main__" maka artinya quotes.py itu merupakan file utama yang sedang dijalankan.
+    
+    Sehingga ketika quotes.py di import di file lain(contoh.py) maka section if ini tidak akan berjalan.
+    
+    Magic Variable
+        __name__: merupakan nama file di dalam sistem python.
+        __main__: merupakan nama file yang sedang dijalankan
+    """
+    print("Topik 2 - Quotes Generator")
+    main()
